125652838a5a08648c969b10d889a375009975f4
Serhii Stanislav <serhiistanislav@gmail.com>
2022-06-03 19:43:58 +0300

    adds initial markup

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,60 @@
+import { useEffect, useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import TokenGenerator from './components/TokenGenerator';
+import { connect as log } from './logs/app-log';
 
+const App = () => {
+  return (
+    <Context>
+      <Content />
+    </Context>
+  );
+};
 
-function App() {
-  return <div>App</div>;
-}
+const Context = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet; // Testnet, Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  useEffect(() => log.app(wallets, endpoint), [endpoint, wallets]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+const Content = () => {
+  return (
+    <>
+      <TokenGenerator />
+    </>
+  );
+};
 
 export default App;
+
+/*
+const wallets = useMemo(
+  () => [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter({ network }),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+    new SolletWalletAdapter({ network }),
+    new SolletExtensionWalletAdapter({ network }),
+  ],
+  [network]
+);
+*/
--- notes/token-generator.js
+++ notes/token-generator.js
@@ -1,6 +1,5 @@
 /*
 
-npm install -g npm@8.12.1
 yarn create-react-app .
 
 */
