d1cc1b3fdeb672eb7bd901cf97ff03e340279d3f
Serhii Stanislav <serhiistanislav@gmail.com>
2022-06-04 20:24:41 +0300

    bugfix

--- notes/token-generator.js
+++ notes/token-generator.js
@@ -10,12 +10,18 @@ git clone https://github.com/itstoreall/token-generator.git
 
 open in IDE
 
-cd app; yarn 
-or 
-make yarn
+The beast way:
+1. $ make env 
+2. $ GENERATE_SOURCEMAP=false
+3. press control + D
+4. $ make yarn
+5. $ make app
 
-cd app; yarn start 
-or 
-make app
+An alternative way:
+1. $ cd app; cat > .env
+2. $ GENERATE_SOURCEMAP=false
+3. press control + D
+4. $ cd app; yarn 
+5. $ yarn start
 
 */
