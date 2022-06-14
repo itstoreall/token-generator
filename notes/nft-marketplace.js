18641850aa28e411135e8882836f902d498188c3
Serhii Stanislav <serhiistanislav@gmail.com>
2022-06-14 10:18:26 +0300

    adds some code and img

---
+++ notes/nft-marketplace.js
@@ -0,0 +1,36 @@
+/*
+
+# NFT Marketplace
+
+---
+
+## Rust (nft-marketplace)
+
+cd contracts
+anchor init nft-marketplace
+
+add two dependencies to the nft-marketplace/programs/nft-marketplace/Cargo.toml:
+mpl-token-metadata = { version="1.2.5", features = [ "no-entrypoint" ] }
+anchor-spl = "0.24.2"
+
+---
+
+## Client
+
+touch nft-marketplace/app/app.ts
+enter the code
+
+---
+
+## Deploy
+
+anchor build
+anchor deploy
+
+---
+
+## Sources:
+
+https://docs.metaplex.com/about/terminology
+
+*/
